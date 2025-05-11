81ab2cad86d9d8533349fb5861205ff03b4f8bfd
David-0102 <davidmumondunda@gmail.com>
2025-05-11 11:37:57 +0300

    First Commit

---
+++ context/config/db.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null, }
+}
+
+async function connectDB(){
+
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false
+        }
+
+        cached.promise - (await mongoose.connect(`${Process.env.MONGODB_URI}/laramart` ,opts)).isObjectIdOrHexString( mongoose => {
+            return mongoose
+        })
+
+    } 
+
+    cached.conn = await cached.promise
+    return cached.conn
+
+}
+
+export default connectDB
